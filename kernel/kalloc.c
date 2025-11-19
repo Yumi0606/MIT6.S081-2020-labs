8f2908457a1d295b7641d0495448584ff13192e4
Yumi0606 <1015622964@qq.com>
2025-11-19 14:29:17 +0800

    lab6

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,20 +23,28 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct ref_stru {
+  struct spinlock lock;
+  int cnt[PHYSTOP / PGSIZE];  // 每个物理页一个计数
+} ref;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref");
   freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for (; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+        ref.cnt[(uint64)p / PGSIZE] = 1; // 初始化为1
+        kfree(p);
+    }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +59,19 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  acquire(&ref.lock);
+  if (--ref.cnt[(uint64)pa / PGSIZE] == 0) {
+    release(&ref.lock);
 
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    r = (struct run*)pa;
+    memset(pa, 1, PGSIZE); // 填充垃圾值
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  } else {
+      release(&ref.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +84,72 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    acquire(&ref.lock);
+    ref.cnt[(uint64)r / PGSIZE] = 1;  // 初始化引用计数为1
+    release(&ref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+int krefcnt(void *pa) {
+  return ref.cnt[(uint64)pa / PGSIZE];
+}
+
+int kaddrefcnt(void* pa) {
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return -1;
+  acquire(&ref.lock);
+  ++ref.cnt[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  return 0;
+}
+
+int cowpage(pagetable_t pagetable, uint64 va) {
+  if (va >= MAXVA) return -1;
+  pte_t *pte = walk(pagetable, va, 0);
+  if (pte == 0 || (*pte & PTE_V) == 0) return -1;
+  return (*pte & PTE_F) ? 0 : -1; // 返回 0 表示是 COW 页
+}
+
+void *
+cowalloc(pagetable_t pagetable, uint64 va)
+{
+    if (va % PGSIZE != 0) return 0;
+
+    uint64 pa = walkaddr(pagetable, va);
+    if (pa == 0) return 0;
+
+    pte_t *pte = walk(pagetable, va, 0);
+    if (pte == 0) return 0;
+
+    if (krefcnt((void*)pa) == 1) {
+        // 只有一个引用，直接设为可写
+        *pte |= PTE_W;
+        *pte &= ~PTE_F;
+        return (void*)pa;
+    } else {
+        // 多个引用，分配新页，拷贝数据
+        char *mem = kalloc();
+        if (mem == 0) return 0;
+
+        memmove(mem, (char*)pa, PGSIZE);
+
+        *pte &= ~PTE_V; // 先取消映射，避免 remap 冲突
+
+        if (mappages(pagetable, va, PGSIZE, (uint64)mem,
+                     (PTE_FLAGS(*pte) | PTE_W) & ~PTE_F) != 0) {
+            kfree(mem);
+            *pte |= PTE_V; // 恢复原 PTE
+            return 0;
+        }
+
+        kfree((void*)pa); // 减少旧页引用，kfree 内部会判断是否为0
+        return mem;
+    }
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -308,31 +308,32 @@ uvmfree(pagetable_t pagetable, uint64 sz)
 int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
-  pte_t *pte;
-  uint64 pa, i;
-  uint flags;
-  char *mem;
+    pte_t *pte;
+    uint64 pa, i;
+    uint flags;
 
-  for(i = 0; i < sz; i += PGSIZE){
-    if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
-    if((*pte & PTE_V) == 0)
-      panic("uvmcopy: page not present");
-    pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
+    for (i = 0; i < sz; i += PGSIZE) {
+        if ((pte = walk(old, i, 0)) == 0)
+            panic("uvmcopy: pte should exist");
+        if ((*pte & PTE_V) == 0)
+            panic("uvmcopy: page not present");
+        pa = PTE2PA(*pte);
+        flags = PTE_FLAGS(*pte);
+
+        if (flags & PTE_W) {
+            // 可写页：设为只读 + COW
+            flags = (flags & ~PTE_W) | PTE_F;
+            *pte = PA2PTE(pa) | flags;
+        }
+
+        if (mappages(new, i, PGSIZE, pa, flags) != 0) {
+            uvmunmap(new, 0, i / PGSIZE, 1);
+            return -1;
+        }
+
+        kaddrefcnt((void*)pa); // 增加引用计数
     }
-  }
-  return 0;
-
- err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
-  return -1;
+    return 0;
 }
 
 // mark a PTE invalid for user access.
@@ -359,6 +360,13 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
+
+    // 处理COW页面的情况
+    if(cowpage(pagetable, va0) == 0) {
+      // 更换目标物理地址
+      pa0 = (uint64)cowalloc(pagetable, va0);
+    }
+
     if(pa0 == 0)
       return -1;
     n = PGSIZE - (dstva - va0);
